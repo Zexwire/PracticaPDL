82a90607e3e249e4688d49952084d4d17c100c9b
Jade <zxmarcos04@gmail.com>
2024-10-07 20:05:03 +0200

    added catches and a rough sketch of scan

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,19 +1,32 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.FileWriter;
 
 public class Lexer{
+    BufferedReader reader;
+    TSHandler tsHandler;
 
-	public Lexer(String string) {
-		//TODO Auto-generated constructor stub
-	}
+    public Lexer(String fileName, TSHandler tsHandler) throws FileNotFoundException {
+        reader = new BufferedReader(new FileReader(fileName));
+        this.tsHandler = tsHandler;
+    }
 
-	public Pair<Token, Object> scan() throws LexerException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'scan'");
+	public Pair<Token, Object> scan() throws LexerException, IOException {
+		int c = reader.read();
+		while(c != -1 && Character.isWhitespace(c)){
+			c = reader.read();
+		}
+		if(c == -1){
+			return new Pair<Token, Object>(Token.EOF, null);
+		}
+		switch(c){
+			case '*':
+				return new Pair<Token, Object>(Token.MULT, null);
+			//TODO: completar con los otros tokens
+			default:
+		}
+		//FIXME: placeholder para evitar error de compilación
+		return null;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
@@ -5,10 +7,10 @@ public class Main {
 		//Para la primera entrega pediremos desde aquí los tokens
 		//al lexer y los pasaremos a un archivo de texto
 		ArrayList<Pair<Token, Object>> tokens = new ArrayList<Pair<Token, Object>>();
-		Lexer lexer = new Lexer("codigo_fuente.txt");
 		TSHandler tsHandler = new TSHandler();
 
 		try {
+			Lexer lexer = new Lexer("codigo_fuente.txt", tsHandler);
 			Pair<Token, Object> token = lexer.scan();
 			while(token.getKey() != Token.EOF && token.getKey() != null){
 				tokens.add(token);
@@ -16,6 +18,12 @@ public class Main {
 			}
 			//TODO: guardar los tokens en un archivo de texto
 			//TODO: guardar la tabla de símbolos en un archivo de texto
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			tokens.clear();
+			//TODO: mirar como se hacia para printear el mensaje de la excepción
+			e.printStackTrace();
 		} catch (LexerException e) {
 			tokens.clear();
 			//TODO: mirar como se hacia para printear el mensaje de la excepción
