79cdaa5464aa84042ad4861e58f62f45306244c2
Sanguis99 <119451252+Sanguis99@users.noreply.github.com>
2024-10-10 16:47:51 +0200

    whiteSpaces() function added
    Mirar como solucionar lo de leer siempre

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,31 +4,27 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Lexer{
-    BufferedReader reader;
-    TSHandler tsHandler;
-    int linea_fichero_fuente;
+    private BufferedReader reader;
+    private TSHandler tsHandler;
+    private int linea_fichero_fuente;
+    private int c;
 
-    public Lexer(String fileName, TSHandler tsHandler) throws FileNotFoundException {
+    public Lexer(String fileName, TSHandler tsHandler) throws FileNotFoundException, IOException {
         reader = new BufferedReader(new FileReader(fileName));
         this.tsHandler = tsHandler;
         linea_fichero_fuente = 1;
+        c = reader.read();
     }
 
 	public Pair<Token, Object> scan() throws LexerException, IOException {
-		int c = reader.read();
-		//Desde aquí
-		while(c != -1 && Character.isWhitespace(c)){ //Estado 0 del AFD
-			c = reader.read();
-		}
-		if(c == -1){
-			return new Pair<Token, Object>(Token.EOF, null);
+		Pair<Token, Object> token = whiteSpaces();
+		if(token != null){
+			return token;
 		}
 		//TODO:Ver si hay una forma más sencilla de comprobar que el caracter es "/"
 		else if(c == "/".codePointAt(0)){ // Paso del estado 0 al 1 del AFD
-			comentario(c);
+			comentario();
 		}
-		//Hasta aquí hay que repetirlo cuando se termina de leer un comentario
-		//TODO: una función auxiliar?
 		switch(c){
 			case '*':
 				return new Pair<Token, Object>(Token.MULT, null);
@@ -36,10 +32,20 @@ public class Lexer{
 			default:
 		}
 		//FIXME: placeholder para evitar error de compilación
+		return token;
+	}
+	
+	private Pair<Token, Object> whiteSpaces() throws IOException { //Estado 0 del AFD y EOF
+		while(c != -1 && Character.isWhitespace(c)){ 
+			c = reader.read();
+		}
+		if(c == -1){
+			return new Pair<Token, Object>(Token.EOF, null);
+		}
 		return null;
 	}
 	
-	private void comentario(int c) throws LexerException, IOException{ //Inicio: estado 1 del AFD
+	private Pair<Token, Object> comentario() throws LexerException, IOException{ //Inicio: estado 1 del AFD
 		//Si no viene un * después de un /
 		c = reader.read();
 		if(c != "*".codePointAt(0)) {
@@ -58,14 +64,24 @@ public class Lexer{
 				}//Paso del estado 3 al estado 2: Si no ha terminado de leer
 			}
 		}
-	}
-	
-	private Pair<Token, Object> words(int c){ //Estado 4 del AFD
-		Pair<Token, Object> token = null;
+		Pair<Token, Object> token = whiteSpaces();
+		if(token != null){
+			return token;
+		}
 		return token;
 	}
 	
-	private Pair<Token, Object> cteCadena(int c) throws IOException{ //Estado 5 del AFD
+	private Pair<Token, Object> words(){ //Estado 4 del AFD
+		Pair<Token, Object> token = null;
+		String str = "";
+		for(char ch: Character.toChars(c)) {
+			str = str + ch;
+		}
+		
+		return token;
+	}
+	
+	private Pair<Token, Object> cteCadena() throws IOException{ //Estado 5 del AFD
 		Pair<Token, Object> token = null;
 		String str = "";
 		c = reader.read();
@@ -79,12 +95,12 @@ public class Lexer{
 		return token;
 	}
 	
-	private Pair<Token, Object> cteEntera(int c){ //Estado 6 del AFD
+	private Pair<Token, Object> cteEntera(){ //Estado 6 del AFD
 		Pair<Token, Object> token = null;
 		return token;
 	}
 	
-	private Pair<Token, Object> otherChars(int c) throws LexerException, IOException{ //Estado 0 al 10, estado 11 y manejo de caracteres no validos
+	private Pair<Token, Object> otherChars() throws LexerException, IOException{ //Estado 0 al 10, estado 11 y manejo de caracteres no validos
 		Pair<Token, Object> token = null;
 		//Si no se puede con un switch se hace con if seguidos y ya
 		switch(c) {
