154be563ab1814f739e2102dcfec9cb7ab0ed249
Sanguis99 <retroordenador123@gmail.com>
2025-01-13 18:44:35 +0100

    Hechos errores del Sintactico y Semantico

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		TSHandler tsHandler = new TSHandler();
 
 		try {
-			Parser parser = new Parser("CodigoFalloLexer.txt", tsHandler);
+			Parser parser = new Parser("CodigoFalloSemantico.txt", tsHandler);
 			parser.parse();
 			tsHandler.toFile("tabla_simbolos.txt");
 		} catch (FileNotFoundException e ) {
